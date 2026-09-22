9eaccc9d9ee806145d1146c1ed927d3c4631cef7
Abdelmonsef Mohammed <abdomohammed929@gmail.com>
2026-09-22 18:55:06 +0300

    chore:Setup quality tools.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,11 +1,7 @@
 <?php
 
-use App\Http\Middleware\HandleAppearance;
-use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
